0f6e06772db7838d893a33c6e978f3f3fa18f797
thoriqulaziz <127114714+thoriqulaziz@users.noreply.github.com>
2023-11-30 11:51:48 +0700

    Add files via upload

---
+++ codelab5/app/Config/DatabaseConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\Config;
+
+class DatabaseConfig
+{
+    // SETTING KONFIGURASI DATABASE KALIAN
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database_name = "codelab";
+    public $port = 3306;
+}
